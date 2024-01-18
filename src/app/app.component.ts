37dccddfc9fc55fa077997b3560aa26260310cd7
NicoC016 <nicoconil2018@gmail.com>
2024-01-17 21:49:00 -0300

    Create landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.sass'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('300ms', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        animate('300ms', style({ opacity: 0 })),
+      ]),
+    ]),
+  ]
 })
 export class AppComponent {
-  title = 'desfileSilviModas';
+  brands = [
+    {
+      name: 'Silvi Modas',
+      image: 'url-a-la-imagen-1.jpg',
+    },
+    {
+      name: 'Ropa Deportiva Pro',
+      image: 'url-a-la-imagen-2.jpg',
+    },
+    {
+      name: 'Ropa Deportiva Pro',
+      image: 'url-a-la-imagen-2.jpg',
+    },
+    {
+      name: 'Ropa Deportiva Pro',
+      image: 'url-a-la-imagen-2.jpg',
+    },
+    {
+      name: 'Ropa Deportiva Pro',
+      image: 'url-a-la-imagen-2.jpg',
+    },
+
+    // Agrega más marcas según sea necesario
+  ];
+  
 }
